fad41d8c264613577bed2172d045786b06195153
remy chaverot <remy.chaverot@epitech.eu>
2020-03-12 18:23:34 +0300

    push homework one

--- hw/hw1/js/index.js
+++ hw/hw1/js/index.js
@@ -1,2 +1,18 @@
 const g = document.querySelector('g');
 const circle = document.querySelector('circle');
+
+document.getElementById("mood").checked = true;
+
+function changeMood(thecheckbox, Moodlabel, svgPath) {
+
+    var checkboxvar = document.getElementById(thecheckbox);
+    var valueLabel = document.getElementById(Moodlabel);
+    if (!checkboxvar.checked) {
+        valueLabel.innerHTML = "Sad";
+        document.getElementById('svg').setAttribute('d','M150 200 Q 225 120 300 200');
+    }
+    else {
+        valueLabel.innerHTML = "Happy";
+        document.getElementById('svg').setAttribute('d','M150 200 Q 225 300 300 200');
+    }
+}
